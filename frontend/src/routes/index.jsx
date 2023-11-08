597f9bc0cec64c10b45275eacbfae4c3c4c863b6
SudhanshuuRanjan <sudhanshuranjan2k18@gmail.com>
2023-11-08 12:44:56 +0530

    � (setup) : Project setup

---
+++ frontend/src/routes/index.jsx
@@ -0,0 +1,43 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
+import { HomePage } from "../pages"
+import ScrollToTop from '../hooks/useScrollToTop';
+import ErrorBoundary from '../components/ErrorBoundary';
+import NavBar from "../components/NavBar";
+import Footer from "../components/Footer";
+
+const index = () => {
+
+    const routes = [
+        {
+            path: "/",
+            element: <HomePage />,
+        },
+        {
+            path: "*",
+            element: <h1>Not Found</h1>
+        }
+    ]
+
+
+
+    return (
+        <Router>
+            <ErrorBoundary>
+                <ScrollToTop />
+                <NavBar />
+                <Routes>
+                    {routes.map((route, idx) =>
+                        <Route
+                            key={idx}
+                            path={route.path}
+                            element={route.element}
+                        />
+                    )}
+                </Routes>
+                <Footer />
+            </ErrorBoundary>
+        </Router>
+    )
+}
+
+export default index
