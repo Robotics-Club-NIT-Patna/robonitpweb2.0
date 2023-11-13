1895b8f34a51400e061d50f5779710c858e53e01
lipi150 <112479235+lipi150@users.noreply.github.com>
2023-11-14 00:05:48 +0530

    navbar

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -3,12 +3,18 @@ import { Link } from 'react-router-dom'
 
 const NavBar = () => {
   return (
-    <div className='flex justify-between w-full bg-rose-600 py-2 px-6'>
-      <Link to="/"><div>Logo</div></Link>
-      <div className='flex gap-5'>
-        <Link to="/about">About</Link>
-        <Link to="/team">Team</Link>
-      </div>
+    <div className='flex justify-between w-full bg-#020617 py-2 px-6 items-center text-white cursor-pointer'>
+        <div>ROBOTICS</div>
+        <div className='flex gap-5 '>
+             <div className=' p-1 flex justify-center rounded-lg   hover:bg-white  hover:text-black '>About</div>
+             <div className=' p-1 flex justify-center rounded-lg hover:bg-white  hover:text-black'>Events</div>
+             <div className=' p-1 flex justify-center rounded-lg hover:bg-white  hover:text-black'>News</div>
+             <div className=' p-1 flex justify-center rounded-lg hover:bg-white  hover:text-black'>Projects</div>
+             <div className=' p-1 flex justify-center rounded-lg hover:bg-white  hover:text-black'>Team</div>
+             <div className=' p-1 flex justify-center rounded-lg hover:bg-white  hover:text-black'>Team</div>
+
+        </div>
+        <div className='p-2 bg-white text-black rounded-lg'>Get Started</div>
     </div>
   )
 }
