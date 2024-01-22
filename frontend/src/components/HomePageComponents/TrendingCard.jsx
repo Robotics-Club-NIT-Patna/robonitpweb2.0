91b27fa0fee4b986e617dc219fe7c6cf6c09abdd
iamshobh <iamshobhraj@gmail.com>
2024-01-22 11:50:28 +0530

    added trendingCard

--- frontend/src/components/HomePageComponents/TrendingCard.jsx
+++ frontend/src/components/HomePageComponents/TrendingCard.jsx
@@ -1,13 +1,27 @@
-import React from 'react'
+import Buttons from "../Buttons";
 
-const TrendingCard = () => {
+const TrendingCard = ({ imageUrl, title, description, date }) => {
   return (
     <>
-      <div>
-           Trending cards
+      <div className="flex flex-col gap-6 group">
+        <div className="rounded-3xl overflow-hidden items-center justify-center h-[20rem] w-[24rem] flex mb-4">
+          <img
+            srcSet={imageUrl}
+            alt="banner"
+            className="transition scale-125 duration-1000 group-hover:scale-150 object-cover "
+            height={440}
+            width={440}
+          ></img>
+        </div>
+        <p className="text-lg leading-6 text-gray-600 font-semibold">{title}</p>
+        <p className="text-3xl font-semibold">{description}</p>
+        <p className="text-lg text-gray-600 font-semibold">{date}</p>
+        <Buttons buttonText="Read More" />
       </div>
     </>
-  )
-}
+  );
+};
 
-export default TrendingCard
+export default TrendingCard;
+
+
---
+++ frontend/src/trendingData.js
@@ -0,0 +1,30 @@
+const trendingData = [
+    {
+        id:1,
+        imageUrl:'https://familyindustrieslive.com/wp-content/uploads/2023/08/IMG_7517.jpg',
+        title: 'Event Merch, Personalized Products, Swag',
+        description: 'Exclusive Event Merch Solutions: Real-Time Apparel Printing for Corporate Events',
+        date: 'Jan 9, 2024'
+    },
+
+    {
+        id:2,
+        imageUrl:'https://familyindustrieslive.com/wp-content/uploads/2023/12/5ECB52DE-81A7-4422-984D-0C7647ABF030.png',
+        title:'Experience Marketing, Event Merch, Live Activation, Event Marketing, Promotional Merch, Event Planning ',
+        description:'Innovative On-Site Merchandising: Transform Your Event Experience and Profits',
+        date:'Dec 18, 2023',
+    },
+    
+    {
+        id:3,
+        imageUrl:'https://familyindustrieslive.com/wp-content/uploads/2023/10/IMG_7247.jpg',
+        title:'Event Activations, Experiential Marketing, Immersive Marketing, Event Merch, Swag ',
+        description:'Boosting Brand Visibility: Innovative On-Site Merchandising',
+        date:'Nov 6, 2023',
+    }
+    
+];
+
+export default trendingData;
+
+
