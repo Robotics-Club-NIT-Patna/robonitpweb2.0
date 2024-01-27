b0491e7ded7ecda91ce6cc1cfdaef95119be009b
lipi150 <112479235+lipi150@users.noreply.github.com>
2024-01-27 12:27:11 +0530

    edited vite config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
   },
   build: {
      rollupOptions: {
-        external: ['react-scroll-parallax']
+        external: ['react-scroll-parallax'],
      }
-  }
+  },
 })
