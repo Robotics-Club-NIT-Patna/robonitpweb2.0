e0c09162454a5127bb640ff14179e892a7465e8c
lipi150 <112479235+lipi150@users.noreply.github.com>
2024-04-22 01:40:07 +0530

    edit-2

--- frontend/src/components/HomePageComponents/RoboExperience/Button.jsx
+++ frontend/src/components/HomePageComponents/RoboExperience/Button.jsx
@@ -2,7 +2,7 @@ import React from "react";
 function Buttonrobo ({buttontext, width}) {
     return (
         <>
-        <div className={`rectangle relative inline-block pl-12 pr-12 h-16 flex justify-center w-max items-center z-10 font-bold text-2xl bg-white rounded-2xl text-black cursor-pointer overflow-hidden hover:text-slate-100 hover-bg-slate-200`}>
+        <div className={`rectangle relative   h-16 flex justify-center w-${width} items-center z-10 font-bold text-2xl bg-white rounded-2xl text-black cursor-pointer overflow-hidden hover:text-slate-100 hover-bg-slate-200`}>
         {buttontext}
       </div>
         </>
